ff28aa8b48048f6ee0f88d2ba14c355ec6fa4a21
SERIN SAJI <44175778+serinsajiammu@users.noreply.github.com>
2020-05-14 11:34:28 +0530

    Add files via upload

---
+++ full.php
@@ -0,0 +1,8 @@
+<html>
+	<body>
+		Welcome <?php echo $_GET["myname"]; ?><br>
+		Your email address is: <?php echo $_GET["myEmail"]; ?><br>
+		Your phone number is: <?php echo $_GET["myphn"]; ?><br>
+		Your address is: <?php echo $_GET["comment"]; ?><br>
+	</body>
+</html>
